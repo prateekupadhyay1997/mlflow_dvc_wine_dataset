7edc80647f843adf0c02d5c89f2d3608975e823f
prateekupadhyay1997 <prateekupadhyay1997@gmail.com>
2022-08-05 10:42:28 +0530

    tox added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a wine Q package", 
+    author="prateek", 
+    packages=find_packages(),
+    license="MIT"
+)
+
